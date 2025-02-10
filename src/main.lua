4ee5e36f8d9a793d76077edcb0cbedec8029b9fd
Leonardo Agrio <leo13nardo84@gmail.com>
2025-02-10 08:42:26 -0500

    improving the output

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,13 @@
 local parser = require("parser")
 local analyzer = require("analizer")
-local file = "../data/your_instagram_activity/messages/inbox/ksenia_1007825067465099/message_1.json"
+local file = "../data/your_instagram_activity/messages/inbox/alejandro_17938649702708022/message_1.json"
 local chat_data = parser.read_json(file)
 
 print("Chat entre:")
 for _, person in ipairs(chat_data.participants) do 
     print("-", person.name)
 end
+
 
 print("Message in the conversation:")
 for _, message in ipairs(chat_data.messages) do 
@@ -18,8 +19,9 @@ end
 --contar mensajes
 local count = analyzer.count_messages(chat_data)
 
+print("------------------------------------------")
 print("messages send by each one")
 
 for name, quantity in pairs(count) do 
-    print(name .. ": " .. quantity)
+    print("-",name .. ": " .. quantity)
 end
